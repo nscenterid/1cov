b379b362b36066c071b67cb1e83dde3a6e732c43
nscenterid <73435511+nscenterid@users.noreply.github.com>
2021-10-30 07:31:42 +0700

    Adding Function For Beta Update
    Adding Def Function

--- 1cov.py
+++ 1cov.py
@@ -5,4 +5,7 @@ print('created by NSI Center)
 print(' Present for Hacktoberfest 2021')
 endpoint = 'https://data.covid19.go.id/public/api/update.json'
       
-      
+def icov_data():
+	response = requests.get(endpoint)
+	data = json.loads(response.text)
+	return str(data)      
